fcaade24886af7997c50aeee2202b0a451546882
FSchroeder88 <96183878+FSchroeder88@users.noreply.github.com>
2023-07-05 21:30:26 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './shared/book';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'book-monkey';
+
+  // Wenn kein Buch ausgewählt ist, ist der Wert null. Ist auch der Standardwert zu Anfang.
+  // Klicken wir auf showDetails ist es nicht mehr null sondern book
+
+  book: Book | null = null;
+
+  showList() {
+    this.book = null;
+  }
+
+  showDetails(book: Book) {
+    this.book = book;
+  }
 }
---
+++ src/app/books/book-list/book-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Book } from '../../shared/book';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss'],
+})
+export class BookListComponent {
+  books: Book[] = [];
+  @Output() selectBook = new EventEmitter<Book>();
+
+  constructor() {
+    this.books = [
+      {
+        isbn: '123456',
+        title: 'Tierisch gut kochen',
+        authors: ['Mrs Chimp', 'Mr Gorilla'],
+        published: '2022-06-20',
+        subtitle: 'Rezepte von Affe bis Zebra',
+        thumbnailUrl: 'https://cdn.ng-buch.de/kochen.png',
+        description: 'Immer lecker und gut'
+      },
+      {
+        isbn: '678954',
+        title: 'Backen mit Affen',
+        authors: ['Orang Utan'],
+        published: '2022-07-15',
+        subtitle: 'Bananenbrot und mehr',
+        thumbnailUrl: 'https://cdn.ng-buch.de/backen.png',
+        description: 'Tolle Backtipps für Mensch und Tier'
+      }
+    ];
+  }
+
+  doSelect(book: Book) {
+    this.selectBook.emit(book);
+  }
+}
